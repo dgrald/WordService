48800eaa7d8d8c0cd591b50cc5fe53c56d856461
dgrald <grald.dylan@gmail.com>
2016-09-18 14:50:50 -0600

    fix title

--- src/main/scala/org/dgrald/WordService.scala
+++ src/main/scala/org/dgrald/WordService.scala
@@ -19,7 +19,7 @@ class WordService extends WordServiceStack with FileUploadSupport with FlashMapS
         <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen"></link>
         <script type="text/javascript" src="js/jquery-3.1.0.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
-        <title>Text Correction Result</title>
+        <title>Text Correction Input</title>
         </head>
 
       <body>
