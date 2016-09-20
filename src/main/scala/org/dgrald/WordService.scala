5a3069adb5375601e2d76dae90d09b7fb7bbe6ed
dgrald <grald.dylan@gmail.com>
2016-09-19 19:06:00 -0600

    UI changes and modal

--- src/main/scala/org/dgrald/WordService.scala
+++ src/main/scala/org/dgrald/WordService.scala
@@ -20,94 +20,120 @@ class WordService extends WordServiceStack with FileUploadSupport with FlashMapS
         <script type="text/javascript" src="js/jquery-3.1.0.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
         <title>Text Correction Input</title>
-        </head>
+      </head>
 
       <body>
-      <h2>Input text here:</h2>
-      <form method="post" enctype="multipart/form-data">
-        <textarea class="form-control" name="filecontents" rows="25"></textarea>
-        <input type="checkbox" name="removenewlines" checked="true"/>{removeNewLinesMessage}<br/>
-        <input type="checkbox" name="linebreaks" checked="true"/>{newLinesMessage}<br/>
-        <input type="checkbox" name="asterisks"/>{asterisksMessage}<br/>
-        <div class="panel-group">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h4 class="panel-title">
-                <a data-toggle="collapse" href="#collapse1">{substitutionMessage}</a>
-              </h4>
-            </div>
-            <div id="collapse1" class="panel-collapse collapse">
-              <div class="panel-body">
-                <table>
-                  <tr>
-                    <th>Replace this term:</th>
-                    <th>With this:</th>
-                  </tr>
-                  <tr>
-                    <td><input type="text" name="replace1"></input></td>
-                    <td><input type="text" name="replacement1"></input></td>
-                    <td><input type="checkbox" name="replaceallbutfirst1"/>{replaceAllButFirstMessage}</td>
-                  </tr>
-                  <tr>
-                    <td><input type="text" name="replace2"></input></td>
-                    <td><input type="text" name="replacement2"></input></td>
-                    <td><input type="checkbox" name="replaceallbutfirst2"/>{replaceAllButFirstMessage}</td>
-                  </tr>
-                  <tr>
-                    <td><input type="text" name="replace3"></input></td>
-                    <td><input type="text" name="replacement3"></input></td>
-                    <td><input type="checkbox" name="replaceallbutfirst3"/>{replaceAllButFirstMessage}</td>
-                  </tr>
-                </table>
+        <!-- Button trigger modal -->
+        <div style="padding: 4px; float: right;">
+          <button type="button" class="btn-info btn" data-toggle="modal" data-target="#myModal">Information</button>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">Information</h4>
+              </div>
+              <div class="modal-body">
+                TODO
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
         </div>
-        <input type="submit" class="btn btn-primary blue"/>
-      </form>
-      <div class="panel-group">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4 class="panel-title">
-              <a data-toggle="collapse" href="#collapse2">File Option</a>
-            </h4>
-          </div>
-          <div id="collapse2" class="panel-collapse collapse">
-            <div class="panel-body">
-              <form method="post" enctype="multipart/form-data">
-              <input type="file" name="thefile" />
-              <input type="checkbox" name="linebreaks" checked="true"/>{newLinesMessage}
-              <input type="checkbox" name="removenewlines" checked="true"/>{removeNewLinesMessage}
-                <input type="checkbox" name="asterisks"/>{asterisksMessage}<br/>
-                <table>
-                <tr>
-                  <th>Replace this term:</th>
-                  <th>With this:</th>
-                </tr>
-                <tr>
-                  <td><input type="text" name="replace1"></input></td>
-                  <td><input type="text" name="replacement1"></input></td>
-                  <td><input type="checkbox" name="replaceallbutfirst1"/>{replaceAllButFirstMessage}</td>
-                </tr>
-                <tr>
-                  <td><input type="text" name="replace2"></input></td>
-                  <td><input type="text" name="replacement2"></input></td>
-                  <td><input type="checkbox" name="replaceallbutfirst2"/>{replaceAllButFirstMessage}</td>
-                </tr>
-                <tr>
-                  <td><input type="text" name="replace3"></input></td>
-                  <td><input type="text" name="replacement3"></input></td>
-                  <td><input type="checkbox" name="replaceallbutfirst3"/>{replaceAllButFirstMessage}</td>
-                </tr>
-              </table>
-              <input type="submit" class="btn btn-primary blue"/>
-            </form>
+        <!-- Actual body !-->
+        <div style="padding-left: 6px; padding-right: 15px;">
+          <h3>Input text here:</h3>
+          <form method="post" enctype="multipart/form-data">
+            <textarea class="form-control" name="filecontents" rows="20"></textarea>
+            <input type="checkbox" name="removenewlines" checked="true"/>{removeNewLinesMessage}<br/>
+            <input type="checkbox" name="linebreaks" checked="true"/>{newLinesMessage}<br/>
+            <input type="checkbox" name="asterisks"/>{asterisksMessage}<br/>
+            <div class="panel-group">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" href="#collapse1">{substitutionMessage}</a>
+                  </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    <table>
+                      <tr>
+                        <th>Replace this term:</th>
+                        <th>With this:</th>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace1"></input></td>
+                        <td><input type="text" name="replacement1"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst1"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace2"></input></td>
+                        <td><input type="text" name="replacement2"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst2"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace3"></input></td>
+                        <td><input type="text" name="replacement3"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst3"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
               </div>
+            </div>
+            <input type="submit" class="btn btn-primary blue"/>
+          </form>
+          <div class="panel-group">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" href="#collapse2">File Option</a>
+                </h4>
+              </div>
+              <div id="collapse2" class="panel-collapse collapse">
+                <div class="panel-body">
+                  <form method="post" enctype="multipart/form-data">
+                    <input type="file" name="thefile" />
+                    <input type="checkbox" name="linebreaks" checked="true"/>{newLinesMessage}
+                    <input type="checkbox" name="removenewlines" checked="true"/>{removeNewLinesMessage}
+                    <input type="checkbox" name="asterisks"/>{asterisksMessage}<br/>
+                    <table>
+                      <tr>
+                        <th>Replace this term:</th>
+                        <th>With this:</th>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace1"></input></td>
+                        <td><input type="text" name="replacement1"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst1"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace2"></input></td>
+                        <td><input type="text" name="replacement2"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst2"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                      <tr>
+                        <td><input type="text" name="replace3"></input></td>
+                        <td><input type="text" name="replacement3"></input></td>
+                        <td style="padding-left: 4px;"><input type="checkbox" name="replaceallbutfirst3"/>{replaceAllButFirstMessage}</td>
+                      </tr>
+                    </table>
+                    <input type="submit" class="btn btn-primary blue"/>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-          </body>
-        </html>
+      </body>
+    </html>
   }
 
   post("/") {
@@ -154,16 +180,18 @@ class WordService extends WordServiceStack with FileUploadSupport with FlashMapS
         <title>Text Correction Result</title>
       </head>
       <body>
-    <form method="get" enctype="multipart/form-data">
-      <div>
-        <input type="submit" value="Back" class="btn btn-primary blue"/>
-      </div>
-      <div>
-        <textarea rows="25" wrap="soft" autofocus="true" onfocus="this.select()" class="form-control">{corrected}</textarea>
-      </div>
-    </form>
-        </body>
-      </html>
+        <div style="padding-left: 6px; padding-right: 15px;">
+          <form method="get" enctype="multipart/form-data">
+            <div style="padding: 4px;">
+              <input type="submit" value="Back" class="btn btn-primary blue"/>
+            </div>
+            <div>
+              <textarea rows="25" wrap="soft" autofocus="true" onfocus="this.select()" class="form-control">{corrected}</textarea>
+            </div>
+          </form>
+        </div>
+      </body>
+    </html>
   }
 
 }
