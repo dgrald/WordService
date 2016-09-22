61ba3cc24486c730c366ddd0628c9f21aa4172ce
dgrald <grald.dylan@gmail.com>
2016-09-22 16:53:40 -0600

    undo last commit

--- src/main/scala/org/dgrald/WordService.scala
+++ src/main/scala/org/dgrald/WordService.scala
@@ -145,14 +145,10 @@ class WordService extends WordServiceStack with FileUploadSupport with FlashMapS
       case Some(contents) => contents
       case None =>
         val file = fileParams("thefile")
-//        val fileType = file.getName.split(".").last
-//        if(fileType == "pdf") {
-//          PdfTextParser.getTextFromPdf(file.get()) match {
-//            case Some(text) => text
-//            case _ => throw new Exception("Could not get file")
-//          }
-//        } else {
-          ImageFileTextParser.getTextFromFile(file.get())
+        PdfTextParser.getTextFromPdf(file.get()) match {
+          case Some(text) => text
+          case _ => throw new Exception("Could not get file")
+        }
     }
 
     val newLinesParam = params.getOrElse("linebreaks", "false")
